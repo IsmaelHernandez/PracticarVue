9a2ee0bc957e502bb516972712a01d3b87afd8f7
IsmaelHernandez <ismael.hernandez@upam.edu.mx>
2023-08-29 11:33:35 -0600

    first commit

---
+++ src/components/firstComponents/FirstComponents.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+      <h2>Consumo de API con Axios en Vue.js 3</h2>
+      <div>
+        <ul>
+          <li v-for="item in data" :key="item.id">{{ item.title }}</li>
+        </ul>
+      </div>
+    </div>
+  </template>
+  
+  
+  <script>
+
+  import { ref } from 'vue';
+  import axios from 'axios';
+  
+  export default {
+    name: 'ApiExample',
+    data() {
+      // Estado para almacenar los datos de la API
+      const data = ref([]);
+    
+      // Función para cargar los datos desde la API
+      const consumeData = async () => {
+        const URL = "https://restcountries.com/v3.1/lang/spanish";
+            axios.get(URL)
+            .then((res) => URL.data)
+            .catch((err) => console.log(err.data))
+        
+      };
+
+      console.log(data)
+  
+      return {
+        data,
+        consumeData,
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Estilos específicos para el componente */
+  </style>
+  
