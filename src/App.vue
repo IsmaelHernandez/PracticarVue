b9ef134b6f4b7dfd57464f63a25dbd0fff6d4691
IsmaelHernandez <ismael.hernandez@upam.edu.mx>
2023-08-30 10:37:23 -0600

    corregi el app.use

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
+
 import Counter from "./components/counter/Counter.vue"
+
 </script>
 
 <template>
+  <!-- Componente Counter -->
   <Counter />
+  
 </template>
 
 
--- src/components/counter/Counter.vue
+++ src/components/counter/Counter.vue
@@ -1,43 +1,42 @@
 <template>
-    <div class="containerCounter">
-      <div class="itemCounter">
-          <h1 class="title">Contador</h1>
-          <p class="title">Valor del contador: {{ counter }}</p>
-        <div class="itemBotom">
-          <button class="" @click="increment">Incrementar</button>
-          <button class="" @click="decrement">Decrementar</button>
-          <button class="" @click="reset">Reset</button>
-        </div>
+  <div class="containerCounter">
+    <div class="itemCounter">
+      <h1 class="title">Contador</h1>
+      <p class="title">Valor del contador: {{ counter }}</p>
+      <div class="itemBotom">
+        <button class="" @click="increment">Incrementar</button>
+        <button class="" @click="decrement">Decrementar</button>
+        <button class="" @click="reset">Reset</button>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import Counter from './Counter.vue'
-  import '@/assets/css/counter/counter.css';
-  
-  export default {
-    name: 'Counter ',
-    data() {
-      return {
-        counter: 0
-      };
+  </div>
+</template>
+
+<script>
+import Counter from "./Counter.vue";
+import "@/assets/css/counter/counter.css";
+
+export default {
+  name: "Counter ",
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.counter++;
     },
-    methods: {
-      increment() {
-        this.counter++;
-      },
-      decrement() {
-        this.counter--;
-      },
-      reset() {
-        this.counter = 0;
-      }
-    }
-  };
-  </script>
-  
-  <style>
-  /* Estilos para tu componente */
-  </style>
-  
+    decrement() {
+      this.counter--;
+    },
+    reset() {
+      this.counter = 0;
+    },
+  },
+};
+</script>
+
+<style>
+/* Estilos para tu componente */
+</style>
