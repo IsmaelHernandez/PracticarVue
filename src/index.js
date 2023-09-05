561fd0c64fc38142c18f678d568fcf485ca9984b
Ismael Hernandez <ismael.hernandez@upam.edu.mx>
2023-09-04 22:27:31 -0600

    created-index2 (#4)

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ let num = 0;
 const frutas = ['melon', 'sandia', 'others'];
 
 const frutas_v2 = [...frutas];
+let num2 = 1;
+
+let arr = [0,1,2,3,4,5]
+
+const sumar = (a,b) => {
+    return a + b
+}
+
+console.log(sumar(2,2))
