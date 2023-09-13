ddacfbb3fe54f0a56f446b5f524513de37490664
IsmaelHernandez <ismael.hernandez@upam.edu.mx>
2023-09-13 14:00:18 -0600

    created layout-v1

---
+++ src/components/layout/SidebarComponent.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="wrapper">
+    <!-- Sidebar  -->
+    <nav :class="{ active: isSidebarActive }" id="sidebar">
+      <div class="sidebar-header">
+        <h3>Practicar Vue.js</h3>
+      </div>
+
+      <ul class="list-unstyled components">
+      
+        <li :class="{ active: isActive('/circle') }">
+          <router-link :to="{ name: 'circle' }">Circle</router-link>
+        </li>
+        
+        <li :class="{ active: isActive('/form') }">
+          <router-link :to="{ name: 'form' }">Form</router-link>
+        </li>
+        <li :class="{ active: isActive('/list') }">
+          <router-link :to="{ name: 'list' }">List User</router-link>
+        </li>
+        <li :class="{ active: isActive('/counter') }">
+          <router-link :to="{ name: 'counter' }">Counter</router-link>
+        </li>
+        <li :class="{ active: isActive('/pokedex') }">
+          <router-link :to="{ name: 'pokedex' }">List Data</router-link>
+        </li>
+        <li :class="{ active: isActive('/person') }">
+          <router-link :to="{ name: 'person' }">MapAction</router-link>
+        </li>
+        <li :class="{ active: isActive('/dashboard') }">
+          <router-link :to="{ name: 'dashboard' }">Form User</router-link>
+        </li>
+      </ul>
+    </nav>
+
+    <!-- Page Content  -->
+    <div id="content">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <button @click="toggleSidebar" type="button" id="sidebarCollapse" class="btn btn-info">
+            <i class="fa-solid fa-bars"></i>
+            <span>Toggle Sidebar</span>
+          </button>
+          <button
+            class="btn btn-dark d-inline-block d-lg-none ml-auto"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i class="fas fa-align-justify"></i>
+          </button>
+
+         
+        </div>
+      </nav>
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<script>
+import "@/assets/css/layout/sidebar.css";
+export default {
+  name: "SidebarComponent",
+  data(){
+    return {
+      isSidebarActive: false,
+
+    }
+  },
+  methods: {
+    toggleSidebar() {
+      this.isSidebarActive = !this.isSidebarActive;
+    },
+    isActive(route){
+      return this.$route.path === route;
+    }
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,51 @@
 import { createRouter, createWebHistory } from "vue-router";
+import TemplateComponent from '@/layout/TemplateComponent.vue'
 
 
 const routes = [
   {
     path: '/',
-    name: 'dashboard',
-    component: () => import("@/components/dashboard/Dashboard.vue"),
-  },
-  {
-    path: '/circle',
-    name: 'circle',
-    component: () => import("@/components/circle/Circle.vue"),
-  },
-  {
-    path: '/form',
-    name: 'form',
-    component: () => import("@/components/form/FormComponent.vue"),
-  },
-  {
-    path: '/listUser',
-    name: 'list',
-    component: () => import("@/components/list/ListComponent.vue"),
-  },
-  {
-    path: '/pokedex',
-    name: 'pokedex',
-    component: () => import("@/components/pokedex/view/Pokedex.vue"),
-  },
-  {
-    path: '/counter',
-    name: 'counter',
-    component: () => import("@/components/counter/Counter.vue"),
-  },
-  {
-    path: '/person',
-    name: 'person',
-    component: () => import("@/components/person/Person.vue"),
-  },
+    component: TemplateComponent,
+    children:[
+      {
+        path: '/circle',
+        name: 'circle',
+        component: () => import("@/components/circle/Circle.vue"),
+      },
+      {
+        path: '/form',
+        name: 'form',
+        component: () => import("@/components/form/FormComponent.vue"),
+      },
+      {
+        path: '/listUser',
+        name: 'list',
+        component: () => import("@/components/list/ListComponent.vue"),
+      },
+      {
+        path: '/pokedex',
+        name: 'pokedex',
+        component: () => import("@/components/pokedex/view/Pokedex.vue"),
+      },
+      {
+        path: '/counter',
+        name: 'counter',
+        component: () => import("@/components/counter/Counter.vue"),
+      },
+      {
+        path: '/person',
+        name: 'person',
+        component: () => import("@/components/person/Person.vue"),
+      },
+      {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: () => import("@/components/dashboard/Dashboard.vue"),
+      },
+    ]
+  }
+  
+  
   
 ];
 
